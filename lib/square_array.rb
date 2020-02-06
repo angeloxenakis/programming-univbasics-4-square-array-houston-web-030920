3054541ef6f63c39650407fac9e1e505e73904b8
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-06 19:37:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   counter = 0 
+  squared_array = []
   while counter < array.legnth do
-    p array[counter]**2
+    squared_array.push(array[counter] ** 2)
     counter += 1
+  end
 end
